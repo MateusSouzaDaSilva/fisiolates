2522a123f531dc31b014205f416576b8a3438ec7
Mateus <mateus.ssilva2002@gmail.com>
2025-06-12 13:54:18 -0300

    composer.json corrigido

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,8 @@ Route::middleware([
 
     Route::post('/aluno/{id}/pagou', [AlunoController::class, 'pagou'])->name('aluno.pagou');
 
-    Route::middleware(['auth'])->get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    //Para o register necessitar de autenticação
+    // Route::middleware(['auth'])->get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 });
 
 Route::middleware([
